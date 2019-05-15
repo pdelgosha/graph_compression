f4421af83e22802ca5e5d35d13aea8c1890e9c48
Payam Delgosha <pdelgosha@gmail.com>
2019-05-15 15:03:26 -0700

    Before removing unnecessary files and cleanup

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -1,8 +1,10 @@
 var searchData=
 [
+  ['readme',['README',['../md__r_e_a_d_m_e.html',1,'']]],
   ['random_5fgraph_2ecpp',['random_graph.cpp',['../random__graph_8cpp.html',1,'']]],
   ['random_5fgraph_2eh',['random_graph.h',['../random__graph_8h.html',1,'']]],
   ['random_5fgraph_5ftest',['random_graph_test',['../test_8cpp.html#ade4fa14856f731f14986b8b0ae8e187d',1,'random_graph_test():&#160;test.cpp'],['../test__mp_8cpp.html#ade4fa14856f731f14986b8b0ae8e187d',1,'random_graph_test():&#160;test_mp.cpp']]],
+  ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
   ['report',['report',['../classlogger.html#adbcc380b0ef53e23125f91e1c8f13f4e',1,'logger']]],
   ['report_5fstream',['report_stream',['../classlogger.html#a8490b3400a43c9c6f3cc3cc122f10996',1,'logger']]],
   ['reverse_5ffenwick_5ftree',['reverse_fenwick_tree',['../classreverse__fenwick__tree.html',1,'reverse_fenwick_tree'],['../classreverse__fenwick__tree.html#ab6cfb68e31b66f39f4b98db43c60f71e',1,'reverse_fenwick_tree::reverse_fenwick_tree()'],['../classreverse__fenwick__tree.html#a508722d0bd06d2a591fc34e34b94c905',1,'reverse_fenwick_tree::reverse_fenwick_tree(vector&lt; int &gt;)']]],
